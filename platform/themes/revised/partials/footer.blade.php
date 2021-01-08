1fb78a984024f29823b3c02cac2d5c48faa520fb
lapnguyen <lap.nguyen@vitopmedia.co>
2021-01-08 18:03:25 +0700

    css

--- platform/themes/revised/partials/footer.blade.php
+++ platform/themes/revised/partials/footer.blade.php
@@ -1,5 +1,25 @@
 <div class="row">
-    <div class="footer">footer</div>
+    
+    <div class="footer">
+        <div class="footer_info">
+            <div class="row">
+                <div class="col-md-3 footer_social col">
+                    <div class="social-container">
+                        <i class="fa fa-facebook-square" aria-hidden="true"></i>
+                        <i class="fa fa-google-plus-square" aria-hidden="true"></i>
+                        <i class="fa fa-youtube-play" aria-hidden="true"></i>
+                        <i class="fa fa-youtube-play" aria-hidden="true"></i>
+                    </div>
+                </div>
+                <div class="col-md-4 col"></div>
+                <div class="col-md-2 col"></div>
+                <div class="col-md-3 col"></div>
+            </div>
+        </div>
+        <div class="footer_text">
+            <div>@2020 vinatex international. Designed & Developed by: <strong>Avalon Solution</strong> </div>
+        </div>
+    </div>
     
 </div>
     {!! Theme::footer() !!}
