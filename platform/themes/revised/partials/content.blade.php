049d78bdc16d65b8a7ce6edefb8d835664e4e530
lapnguyen <lapnguyenvitopmedia>
2021-01-11 13:26:05 +0700

    css

--- platform/themes/revised/partials/content.blade.php
+++ platform/themes/revised/partials/content.blade.php
@@ -3,12 +3,21 @@
 </div>
 <div class="row">
     <div class="inner-container">{!! Theme::content() !!}</div>
-    @php
-        
-    @endphp
     <div>
-        -------{{ Request()->parameter }}
-        {{ Request::query('parameter') }}
+        @if (url()->current() == route('public.single') || ($page && $page->template === 'homepage'))
+            <div class="home_content">
+                @php
+                    $cates = get_featured_categories(3);
+                    for($i=0; $i<count($cates); $i++) {
+                        $field = \Botble\Blog\Models\Category::find($cates[$i]->id);
+                        $image = get_field($field, 'image');
+                        echo '<image src="storage/'.$image.'" />';
+                    }
+                    
+                @endphp
+            </div>
+        @endif
+        
     </div>
 </div>
 
