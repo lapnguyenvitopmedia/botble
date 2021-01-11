8307dffbbddcd901d751e81f52ae9ba86f6c13ef
lapnguyen <lapnguyenvitopmedia>
2021-01-11 15:48:24 +0700

    home content

--- platform/themes/revised/partials/content.blade.php
+++ platform/themes/revised/partials/content.blade.php
@@ -6,15 +6,31 @@
     <div>
         @if (url()->current() == route('public.single') || ($page && $page->template === 'homepage'))
             <div class="home_content">
-                @php
-                    $cates = get_featured_categories(3);
-                    for($i=0; $i<count($cates); $i++) {
-                        $field = \Botble\Blog\Models\Category::find($cates[$i]->id);
-                        $image = get_field($field, 'image');
-                        echo '<image src="storage/'.$image.'" />';
-                    }
-                    
-                @endphp
+                <div class="main_title">{{ __('Lastest Collections') }} <a><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a> </div>
+                <div class="home_collection">
+                    <ul>
+                        
+                        @php
+                            $cates = get_featured_categories(3);
+                            for($i=0; $i<count($cates); $i++) {
+                                $field = \Botble\Blog\Models\Category::find($cates[$i]->id);
+                                $image = get_field($field, 'image');
+                                //echo '<image src="storage/'.$image.'" />';
+                                echo '<li style="background-image: url(storage/'.$image.')">
+                                        <div class="cover_title">
+                                            <span class="title">'.$cates[$i]->name.'</span>
+                                            <div class="discover">
+                                                <a href="'.$cates[$i]->url.'">Discover 
+                                                    <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+                                                </a>
+                                                
+                                            </span>
+                                        </div>
+                                    </li>';
+                            }
+                        @endphp
+                    </ul>
+                </div>
             </div>
         @endif
         
